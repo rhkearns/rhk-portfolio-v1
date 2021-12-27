1ea4f2ab4d42e80098997579b9863fb75c1064a3
rhkearns <ryankearns217@gmail.com>
2021-12-27 11:44:38 -0500

    build out contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import LILogo from '../../images/siteLogos/LI-In-Bug.png'
+import GHLogo from '../../images/siteLogos/GitHub-Mark-120px-plus.png'
+import headshot from '../../images/headshot/headshot.jpg'
 
 const Contact = () => {
   return (
-    <h1>Contact</h1>
+    <div className="contact-section">
+      <h1>Contact</h1>
+      <div className='headshot'>
+        <img src={headshot} alt="Ryan Kearns Headshot" />
+      </div>
+      <div className="contact-info">
+        <h4>Email: ryankearns217@gmail.com</h4>
+        <div className="links">
+          <a href="https://www.linkedin.com/in/ryanhkearns/"><img src={LILogo} alt="LinkedIn Logo" /></a>
+          <a href="https://github.com/rhkearns"><img src={GHLogo} alt="GitHub Logo" /></a>
+        </div>
+      </div>
+    </div>
   )
 }
 
