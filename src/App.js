ae67239044563b5da7cd1f67102449bfbf158598
rhkearns <ryankearns217@gmail.com>
2021-12-27 10:56:23 -0500

    set up app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './App.css';
 
+// Components
+import Contact from './components/contact/Contact';
+import Projects from './components/projects/Projects';
+
 function App() {
   return (
-    <h1>Hello, World</h1>
+    <>
+    <div className='landing'>
+      <p className="brand-statement">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit autem architecto eveniet quae minima excepturi eligendi commodi quisquam sequi voluptates?</p>
+    </div>
+    <div className='projects'>
+      <Projects />
+    </div>
+    <div className='contact'>
+      <Contact />
+    </div>
+  </>
   )
 }
 
