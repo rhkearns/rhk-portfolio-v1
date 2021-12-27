d80aab7b57feddb6071289b4b65242c973467023
rhkearns <ryankearns217@gmail.com>
2021-12-27 18:50:51 -0500

    style contacts

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import LILogo from '../../images/siteLogos/LI-In-Bug.png'
 import GHLogo from '../../images/siteLogos/GitHub-Mark-120px-plus.png'
 import headshot from '../../images/headshot/headshot.jpg'
+import envelope from '../../images/siteLogos/Envelope.png'
+import resume from '../../images/siteLogos/Resume.png'
 import './contact.css'
 
 const Contact = () => {
@@ -9,17 +11,13 @@ const Contact = () => {
     <div className="contact-section">
       <div className='headshot-container'>
         <img src={headshot} alt="Ryan Kearns Headshot" className='headshot'/>
+        <h3>Ryan H Kearns</h3>
       </div>
-      <div className="contact-info">
-        <h1 id="header">Contact Information</h1>
-        <h2><a href="https://docs.google.com/document/d/1atdX-pdKJCal04UBLpt59AHstj4MyhqH/edit?usp=sharing&ouid=115905962327170111225&rtpof=true&sd=true">Resume</a></h2>
-        <div className="contacts">
-          <h3 id="email">Email: <a href="mailto:ryankearns217@gmail.com"> ryankearns217@gmail.com</a></h3>
-          <div className="links">
-            <a href="https://www.linkedin.com/in/ryanhkearns/"><img src={LILogo} alt="LinkedIn Logo" id="li-logo"/></a>
-            <a href="https://github.com/rhkearns"><img src={GHLogo} alt="GitHub Logo" id="gh-logo"/></a>
-          </div>
-        </div>
+      <div className="links">
+        <a href="https://docs.google.com/document/d/1atdX-pdKJCal04UBLpt59AHstj4MyhqH/edit?usp=sharing&ouid=115905962327170111225&rtpof=true&sd=true"><img src={resume} alt='resume' id="resume-icon"/></a>
+        <a href="mailto:ryankearns217@gmail.com"><img src={envelope} alt="email" id="email-icon"/></a>
+        <a href="https://www.linkedin.com/in/ryanhkearns/"><img src={LILogo} alt="LinkedIn Logo" id="li-logo"/></a>
+        <a href="https://github.com/rhkearns"><img src={GHLogo} alt="GitHub Logo" id="gh-logo"/></a>
       </div>
     </div>
   )
