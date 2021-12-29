342c239cf9b0e09e5ebbbfaa666b3fe330413c30
rhkearns <ryankearns217@gmail.com>
2021-12-29 16:43:48 -0500

    add project links

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -8,33 +8,40 @@ import quiz from '../../images/projectThumbnails/quiz.png'
 const Projects = () => {
   return (
     <div className='project-list'>
-      <Card className="card" style={{ width: '18rem' }}>
-        <Card.Img variant='top' src={boxitup} className='card-img'/>
-        <Card.Body>
-          <Card.Title className="card-title">Box It Up</Card.Title>
-          <Card.Text className="card-text">
-            A full-stack CRUD app, using Python with the Django web framework, designed to help movers keep track of their many boxes.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="card" style={{ width: '18rem' }}>
-        <Card.Img variant='top' src={readrs} className='card-img'/>
-        <Card.Body>
-          <Card.Title className="card-title">Readrs</Card.Title>
-          <Card.Text className="card-text">
-            A full-stack CRUD app, using the MEN Stack, designed to let book lovers sort and catagorize their books.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="card" style={{ width: '18rem' }}>
-        <Card.Img variant='top' src={quiz} className='card-img'/>
-        <Card.Body>
-          <Card.Title className="card-title">Quiz for the Nerds</Card.Title>
-          <Card.Text className="card-text">
-            A simple quiz game designed to test nerds on their knowledge of the most popular franchises, using vanilla JavaScript.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <a href="https://boxitup-rhk.herokuapp.com/">
+        <Card className="card" style={{ width: '18rem' }}>
+          <Card.Img variant='top' src={boxitup} className='card-img'/>
+          <Card.Body>
+            <Card.Title className="card-title">Box It Up</Card.Title>
+            <Card.Text className="card-text">
+              A full-stack CRUD app, using Python with the Django web framework, designed to help movers keep track of their many boxes.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </a>
+      <a href="https://readrs.herokuapp.com/">
+        <Card className="card" style={{ width: '18rem' }}>
+          <Card.Img variant='top' src={readrs} className='card-img'/>
+          <Card.Body>
+            <Card.Title className="card-title">Readrs</Card.Title>
+            <Card.Text className="card-text">
+              A full-stack CRUD app, using the MEN Stack, designed to let book lovers sort and catagorize their books.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </a>
+      <a href="https://quiz-for-the-nerds.netlify.app/">
+        <Card className="card" style={{ width: '18rem' }}>
+          <Card.Img variant='top' src={quiz} className='card-img'/>
+          <Card.Body>
+            <Card.Title className="card-title">Quiz for the Nerds</Card.Title>
+            <Card.Text className="card-text">
+              A simple quiz game designed to test nerds on their knowledge of the most popular franchises, using vanilla JavaScript.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </a>
+
     </div>
   )
 }
