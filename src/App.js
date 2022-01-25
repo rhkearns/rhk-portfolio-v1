b189976cf3f884972aaf4a4561bedd6d5c077f81
rhkearns <ryankearns217@gmail.com>
2022-01-25 13:24:38 -0500

    add redux, update resume link

--- src/App.js
+++ src/App.js
@@ -4,38 +4,48 @@ import './App.css';
 // Components
 import Contact from './components/contact/Contact';
 import Projects from './components/projects/Projects';
-import html5 from './images/icons/html5.png'
-import css3 from './images/icons/css3.png'
-import javascript from './images/icons/javascript.png'
-import nodejs from './images/icons/nodejs.png'
-import python from './images/icons/python.png'
-import react from './images/icons/react.png'
+import html5 from './images/icons/html5.png';
+import css3 from './images/icons/css3.png';
+import javascript from './images/icons/javascript.png';
+import nodejs from './images/icons/nodejs.png';
+import python from './images/icons/python.png';
+import react from './images/icons/react.png';
+import redux from './images/icons/redux.png';
 
 function App() {
-  return (
-    <div className='body'>
-      <div className='landing'>
-        <h1>Ryan H Kearns</h1>
-        <p className="brand-statement">
-          A thoughtful and solutions-driven software developer, with a passion for creating enjoyable experiences for all. Through attention to detail and creative problem solving, I strive to find new and inventive ways of approaching every situation. Both routine and unusual occurrences bring out my best work, adapting to new situations with the same care I bring to the ordinary. My background in team-oriented organizations has allowed me to learn new skills and perspectives, and coworkers have seen my diligence and reliability in my work.
-        </p>
-        <div className='techs'>
-          <img src={html5} alt='html5'/>
-          <img src={css3} alt='css3'/>
-          <img src={javascript} alt='javascript'/>
-          <img src={nodejs} alt='nodejs'/>
-          <img src={react} alt='react'/>
-          <img src={python} alt='python'/>
-        </div>
-      </div>
-      <div className='projects'>
-        <Projects />
-      </div>
-      <div className='contact'>
-        <Contact />
-      </div>
-    </div>
-  )
+	return (
+		<div className="body">
+			<div className="landing">
+				<h1>Ryan H Kearns</h1>
+				<p className="brand-statement">
+					A thoughtful and solutions-driven software developer, with a passion
+					for creating enjoyable experiences for all. Through attention to
+					detail and creative problem solving, I strive to find new and
+					inventive ways of approaching every situation. Both routine and
+					unusual occurrences bring out my best work, adapting to new situations
+					with the same care I bring to the ordinary. My background in
+					team-oriented organizations has allowed me to learn new skills and
+					perspectives, and coworkers have seen my diligence and reliability in
+					my work.
+				</p>
+				<div className="techs">
+					<img src={html5} alt="html5" />
+					<img src={css3} alt="css3" />
+					<img src={javascript} alt="javascript" />
+					<img src={nodejs} alt="nodejs" />
+					<img src={react} alt="react" />
+					<img src={python} alt="python" />
+					<img src={redux} alt="redux" />
+				</div>
+			</div>
+			<div className="projects">
+				<Projects />
+			</div>
+			<div className="contact">
+				<Contact />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
         <img src={headshot} alt="Ryan Kearns Headshot" className='headshot'/>
       </div>
       <div className="links">
-        <a href="https://drive.google.com/file/d/1Q093oBEnTF30uBxsiWy0EvILTSbQ72RM/view?usp=sharing" target='_blank' rel='noopener noreferrer'><img src={resume} alt='resume' id="resume-icon"/></a>
+        <a href="https://drive.google.com/file/d/1sZk5dnf_b8_w5R7l0NzjIpcjWWXmeA4m/view?usp=sharing" target='_blank' rel='noopener noreferrer'><img src={resume} alt='resume' id="resume-icon"/></a>
         <a href="mailto:ryankearns217@gmail.com" target='_blank' rel='noopener noreferrer'><img src={envelope} alt="email" id="email-icon"/></a>
         <a href="https://www.linkedin.com/in/ryanhkearns/" target='_blank' rel='noopener noreferrer'><img src={LILogo} alt="LinkedIn Logo" id="li-logo"/></a>
         <a href="https://github.com/rhkearns" target='_blank' rel='noopener noreferrer'><img src={GHLogo} alt="GitHub Logo" id="gh-logo"/></a>
