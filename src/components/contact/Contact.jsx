f5efb0a3252f99c47122d2fd26b02e1cd5659111
rhkearns <ryankearns217@gmail.com>
2022-01-31 17:12:46 -0500

    add hover animation to icon links

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,25 +1,54 @@
 import React from 'react';
-import LILogo from '../../images/siteLogos/LI-In-Bug.png'
-import GHLogo from '../../images/siteLogos/GitHub-Mark-120px-plus.png'
-import headshot from '../../images/headshot/headshot.jpg'
-import envelope from '../../images/siteLogos/Envelope-cropped.png'
-import resume from '../../images/siteLogos/Resume-cropped.png'
-import './contact.scss'
+import LILogo from '../../images/siteLogos/LI-In-Bug.png';
+import GHLogo from '../../images/siteLogos/GitHub-Mark-120px-plus.png';
+import headshot from '../../images/headshot/headshot.jpg';
+import envelope from '../../images/siteLogos/Envelope-cropped.png';
+import resume from '../../images/siteLogos/Resume-cropped.png';
+import './contact.scss';
 
 const Contact = () => {
-  return (
-    <div className="contact-section">
-      <div className='headshot-container'>
-        <img src={headshot} alt="Ryan Kearns Headshot" className='headshot'/>
-      </div>
-      <div className="links">
-        <a href="https://drive.google.com/file/d/1sZk5dnf_b8_w5R7l0NzjIpcjWWXmeA4m/view?usp=sharing" target='_blank' rel='noopener noreferrer'><img src={resume} alt='resume' id="resume-icon"/></a>
-        <a href="mailto:ryankearns217@gmail.com" target='_blank' rel='noopener noreferrer'><img src={envelope} alt="email" id="email-icon"/></a>
-        <a href="https://www.linkedin.com/in/ryanhkearns/" target='_blank' rel='noopener noreferrer'><img src={LILogo} alt="LinkedIn Logo" id="li-logo"/></a>
-        <a href="https://github.com/rhkearns" target='_blank' rel='noopener noreferrer'><img src={GHLogo} alt="GitHub Logo" id="gh-logo"/></a>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="contact-section">
+			<div className="headshot-container">
+				<img src={headshot} alt="Ryan Kearns Headshot" className="headshot" />
+			</div>
+			<div className="links">
+				<a
+					href="https://drive.google.com/file/d/1sZk5dnf_b8_w5R7l0NzjIpcjWWXmeA4m/view?usp=sharing"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img src={resume} alt="resume" className="icons" id="resume-icon" />
+				</a>
+				<a
+					href="mailto:ryankearns217@gmail.com"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img src={envelope} alt="email" className="icons" id="email-icon" />
+				</a>
+				<a
+					href="https://www.linkedin.com/in/ryanhkearns/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={LILogo}
+						alt="LinkedIn Logo"
+						className="icons"
+						id="li-logo"
+					/>
+				</a>
+				<a
+					href="https://github.com/rhkearns"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img src={GHLogo} alt="GitHub Logo" className="icons" id="gh-logo" />
+				</a>
+			</div>
+		</div>
+	);
+};
 
-export default Contact
+export default Contact;
